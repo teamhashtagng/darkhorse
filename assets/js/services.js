9ef92f99d1cc3f0875ddb4a19b3f91e8d60d4449
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-02-20 07:00:06 +0100

    remove commented out code

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,10 +1,6 @@
 const materialCarousel = new Swiper(".services-carousel--material", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: true,
-  // },
   navigation: {
     nextEl: ".swiper-button-next--material",
     prevEl: ".swiper-button-prev--material",
@@ -14,10 +10,6 @@ const materialCarousel = new Swiper(".services-carousel--material", {
 const manpowerCarousel = new Swiper(".services-carousel--manpower", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: true,
-  // },
   navigation: {
     nextEl: ".swiper-button-next--manpower",
     prevEl: ".swiper-button-prev--manpower",
@@ -27,10 +19,6 @@ const manpowerCarousel = new Swiper(".services-carousel--manpower", {
 const engineeringCarousel = new Swiper(".services-carousel--engineering", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: true,
-  // },
   navigation: {
     nextEl: ".swiper-button-next--engineering",
     prevEl: ".swiper-button-prev--engineering",
