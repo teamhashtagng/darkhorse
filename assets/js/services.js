f3cfb99f99c1682fe179e4981336cb9654211756
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-02-20 05:50:49 +0100

    add and style materials & engineering services with corresponding carousel functionality

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,12 +1,38 @@
-const servicesCarousel = new Swiper(".services-carousel--material", {
+const materialCarousel = new Swiper(".services-carousel--material", {
   spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next--material",
     prevEl: ".swiper-button-prev--material",
+  },
+});
+
+const manpowerCarousel = new Swiper(".services-carousel--manpower", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next--manpower",
+    prevEl: ".swiper-button-prev--manpower",
+  },
+});
+
+const engineeringCarousel = new Swiper(".services-carousel--engineering", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next--engineering",
+    prevEl: ".swiper-button-prev--engineering",
   },
 });
