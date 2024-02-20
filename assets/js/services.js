4689f1d796ace6d494ecf8163643e489d1147922
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-02-20 04:55:48 +0100

    add functionality for material services slider

---
+++ assets/js/services.js
@@ -0,0 +1,12 @@
+const materialSwiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next--material",
+    prevEl: ".swiper-button-prev--material",
+  },
+});
