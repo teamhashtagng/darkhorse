778413c6bd7f2dfa63f51969570137d1393fd21e
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-03-01 16:24:04 +0100

    Add nav dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector(".nav-toggle");
 const headerNav = document.querySelector(".header__nav");
+const allHeaderNavDropdowns = [
+  ...document.querySelectorAll(".header__nav--dropdown"),
+];
 
 function toggleNav(event) {
   const navToggle = event.currentTarget;
@@ -16,11 +19,36 @@ function toggleNav(event) {
   }
 }
 
+function toggleNavDropdown(event) {
+  if (!event.target.closest(".header__nav--dropdown")) return;
+  const navDropdown = event.target.closest(".header__nav--dropdown");
+  const navDropdownMenu = navDropdown.querySelector("ul");
+  const navDropdownMenuIsHidden = navDropdownMenu.getAttribute("aria-hidden");
+
+  allHeaderNavDropdowns.forEach((headerNavDropdown) => {
+    headerNavDropdown.querySelector("ul").setAttribute("aria-hidden", true);
+    headerNavDropdown.style.marginBottom = 0;
+  });
+
+  if (navDropdownMenuIsHidden === "true") {
+    navDropdownMenu.setAttribute("aria-hidden", false);
+    const navDropdownMenuHeight = getComputedStyle(navDropdownMenu).height;
+    const navDropdownMargin = navDropdownMenuHeight.split("px")[0] - 40;
+    if (window.innerWidth <= 800) {
+      navDropdown.style.marginBottom = `${navDropdownMargin}px`;
+    }
+  } else {
+    navDropdownMenu.setAttribute("aria-hidden", true);
+    navDropdown.style.marginBottom = 0;
+  }
+}
+
 function transitionAfterPageLoad() {
   document.body.classList.remove("no-transition");
 }
 
 navToggle.addEventListener("click", toggleNav);
+headerNav.addEventListener("click", toggleNavDropdown);
 setTimeout(function () {
   transitionAfterPageLoad();
 }, 1);
